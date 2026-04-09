8dc22406e46cc4dc67a9de4f6476f1984556fddc
xufang <fang.xu@intel.com>
2026-04-09 09:29:36 +0800

    fix bug

--- optimum/exporters/openvino/model_patcher.py
+++ optimum/exporters/openvino/model_patcher.py
@@ -7774,7 +7774,7 @@ class LlamaEagle3DecoderLayer(nn.Module):
             hidden_states=hidden_states,
             attention_mask=attention_mask,
             position_ids=position_ids,
-            past_key_value=past_key_values,
+            past_key_values=past_key_values,
             output_attentions=output_attentions,
             position_embeddings=position_embeddings,
             use_cache=use_cache,
